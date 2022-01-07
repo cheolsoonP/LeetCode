c37d3a7d18b5f3a486c68aa61c7da3ef68788194
Park Cheolsoon <mn5724@naver.com>
2022-01-07 15:55:37 +0900

    Time: 32 ms (86.49%), Space: 14.3 MB (73.04%) - LeetHub

--- 819-most-common-word/819-most-common-word.py
+++ 819-most-common-word/819-most-common-word.py
@@ -2,20 +2,28 @@ class Solution:
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
         
         # 1. List comprehenction, Counter Object
-        
-        words = []
-        
-        for word in re.sub(r'[^\w]', ' ', paragraph).lower().split():
-            if word not in banned:
-                words.append(word)
 
+        
+        # 72ms
+                
+        # words = []
+        
+        # for word in re.sub(r'[^\w]', ' ', paragraph).lower().split():
+        #     if word not in banned:
+        #         words.append(word)
+                
+
+        # 
+        words = [word for word in re.sub(r'[^\w]', ' ', paragraph)
+                    .lower().split()
+                        if word not in banned]
+                
+                
         counts = collections.Counter(words)
         
         print(counts)
         
         return counts.most_common(1)[0][0]
         
-#         words = [word for word in re.sub(r'[^/w]', ' ', paragragh)
-#                     .lower().split()
-#                         if word not in banned]
+
         
