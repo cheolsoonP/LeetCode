80eaab2060a4d6e8c063731232feb549601795aa
Park Cheolsoon <mn5724@naver.com>
2022-01-11 22:43:29 +0900

    Time: 122 ms (30.91%), Space: 18.7 MB (84.89%) - LeetHub

---
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -0,0 +1,20 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # 1. heapq 사용
+        
+        heap = []
+        
+        # key - num / value - count
+        freq = collections.Counter(nums)
+        
+        for f in freq:
+            # key - count / value - num
+            # 힙은 키 순서로 정렬된다.
+            # 빈도수 순서로 정렬하기 위해
+            heapq.heappush(heap, (-freq[f], f))
+            
+        top = []
+        for _ in range(k):
+            top.append(heapq.heappop(heap)[1])
+            
+        return top
