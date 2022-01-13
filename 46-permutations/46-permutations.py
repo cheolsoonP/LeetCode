8e31af8dc9890b2c403ce2b0764bf08d65bab0c6
Park Cheolsoon <mn5724@naver.com>
2022-01-13 16:51:30 +0900

    Time: 49 ms (31.09%), Space: 14.5 MB (44.31%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -1,23 +1,28 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        ## ****`
-        results = []
-        prev_elements = []
+#         ## ****`
+#         results = []
+#         prev_elements = []
         
-        def dfs(elements):
+#         def dfs(elements):
             
-            if len(elements) == 0:
-                results.append(prev_elements[:])
+#             if len(elements) == 0:
+#                 results.append(prev_elements[:])
             
             
-            # 순열 생성 재귀 호출
-            for e in elements:
-                next_elements = elements[:]
-                next_elements.remove(e)
+#             # 순열 생성 재귀 호출
+#             for e in elements:
+#                 next_elements = elements[:]
+#                 next_elements.remove(e)
                 
-                prev_elements.append(e)
-                dfs(next_elements)
-                prev_elements.pop()
+#                 prev_elements.append(e)
+#                 dfs(next_elements)
+#                 prev_elements.pop()
         
-        dfs(nums)
-        return results
+#         dfs(nums)
+#         return results
+    
+            
+        # 2. itertool 사용 - 라이브러리 
+        # 구현의 효율성, 성능을 위해 사용했다.  
+        return list(map(list, itertools.permutations(nums)))
