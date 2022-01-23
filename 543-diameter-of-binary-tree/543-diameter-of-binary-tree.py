b3d78dce4a7e9205fff4ed06e67ae2bb570a64aa
Park Cheolsoon <mn5724@naver.com>
2022-01-23 09:59:22 +0900

    Time: 44 ms (79.55%), Space: 16.5 MB (26.86%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.py
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.py
@@ -4,24 +4,25 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
     longest: int = 0
     
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
         
+        
         def dfs(node: TreeNode) -> int:
+            # 노드가 없는 곳은 -1
             if not node:
                 return -1
-            
-            # 왼쪽, 오른쪽의 각 리프 노드까지 탐색
+
             left = dfs(node.left)
             right = dfs(node.right)
             
-            
-            # 가장 긴 경로
+            # 거리
             self.longest = max(self.longest, left + right + 2)
             
-            # 상태 값
+            # 상태값
             return max(left, right) + 1
         
         dfs(root)
