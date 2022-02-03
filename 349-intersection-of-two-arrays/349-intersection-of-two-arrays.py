982a211f0d989d2ee605a814a06781e55b4a2475
Park Cheolsoon <mn5724@naver.com>
2022-02-03 14:16:44 +0900

    Time: 86 ms (18.81%), Space: 14 MB (97.22%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.py
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.py
@@ -1,15 +1,15 @@
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
         
-        # 1 brute force
-        result: Set = set()
+#         # 1 brute force
+#         result: Set = set()
             
-        for n1 in nums1:
-            for n2 in nums2:
-                if n1 == n2:
-                    result.add(n1)
+#         for n1 in nums1:
+#             for n2 in nums2:
+#                 if n1 == n2:
+#                     result.add(n1)
                     
-        return result
+#         return result
     
 #         # 2. binary
 #         result: Set = set()
@@ -20,3 +20,23 @@ class Solution:
 #             if len(nums2) > 0 and len(nums2) > i2 and n1 == nums2[i2]:
 #                 result.add(n1)
 #         return result
+
+        # 3. two pointer
+    
+        result: Set = set()
+            
+        nums1.sort()
+        nums2.sort()
+        
+        i = j = 0
+        
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] < nums2[j]:
+                i += 1
+            elif nums1[i] > nums2[j]:
+                j += 1
+            else:
+                result.add(nums1[i])
+                i += 1
+                j += 1
+        return result
