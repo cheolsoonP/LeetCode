7016a1a001802dbdf56a1234d4e6b6e1259b9931
Park Cheolsoon <mn5724@naver.com>
2022-02-03 14:11:22 +0900

    Time: 120 ms (5.18%), Space: 14.1 MB (97.22%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.py
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.py
@@ -1,22 +1,22 @@
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
         
-#         # 1 brute force
-#         result: Set = set()
-            
-#         for n1 in nums1:
-#             for n2 in nums2:
-#                 if n1 == n2:
-#                     result.add(n1)
-                    
-#         return result
-    
-        # 2. binary
+        # 1 brute force
         result: Set = set()
-        nums2.sort()
-        
+            
         for n1 in nums1:
-            i2 = bisect.bisect_left(nums2, n1)
-            if len(nums2) > 0 and len(nums2) > i2 and n1 == nums2[i2]:
-                result.add(n1)
+            for n2 in nums2:
+                if n1 == n2:
+                    result.add(n1)
+                    
         return result
+    
+#         # 2. binary
+#         result: Set = set()
+#         nums2.sort()
+        
+#         for n1 in nums1:
+#             i2 = bisect.bisect_left(nums2, n1)
+#             if len(nums2) > 0 and len(nums2) > i2 and n1 == nums2[i2]:
+#                 result.add(n1)
+#         return result
