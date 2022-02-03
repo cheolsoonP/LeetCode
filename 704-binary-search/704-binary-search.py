5aa19755c279e3e481524f960b36b8a1823b1a58
Park Cheolsoon <mn5724@naver.com>
2022-02-03 13:41:31 +0900

    Time: 408 ms (18.92%), Space: 15.5 MB (66.27%) - LeetHub

--- 704-binary-search/704-binary-search.py
+++ 704-binary-search/704-binary-search.py
@@ -19,20 +19,27 @@ class Solution:
 #         return binary_search(0, len(nums)-1)
 
     
-        # 2. 반복
-        left, right = 0, len(nums) - 1
+#         # 2. 반복
+#         left, right = 0, len(nums) - 1
         
-        while left <= right:
-            mid = (left + right) // 2
+#         while left <= right:
+#             mid = (left + right) // 2
             
-            if nums[mid] < target:
-                left = mid + 1
-            elif nums[mid] > target:
-                right = mid - 1
-            else:
-                return mid
+#             if nums[mid] < target:
+#                 left = mid + 1
+#             elif nums[mid] > target:
+#                 right = mid - 1
+#             else:
+#                 return mid
         
-        return -1
+#         return -1
         
+        # 3. 이진 검색 모듈
+        index = bisect.bisect_left(nums, target)
+        
+        if index < len(nums) and nums[index] == target:
+            return index
+        else:
+            return -1 
         
         
