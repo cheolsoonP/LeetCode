b615a52e65cc9e76cb44b4dfdcfd3fd8b229e7fd
Park Cheolsoon <mn5724@naver.com>
2022-02-03 14:48:02 +0900

    Time: 169 ms (6.41%), Space: 14.5 MB (84.84%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -1,17 +1,32 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         
-        # 1. two pointer
-        left, right = 0, len(numbers) - 1
-        while not left == right:
-            if numbers[left] + numbers[right] < target:
-                left += 1
-            elif numbers[left] + numbers[right] > target:
-                right -= 1
-            else:
-                # index가 1부터 시작
-                return left + 1, right + 1
+#         # 1. two pointer
+#         left, right = 0, len(numbers) - 1
+#         while not left == right:
+#             if numbers[left] + numbers[right] < target:
+#                 left += 1
+#             elif numbers[left] + numbers[right] > target:
+#                 right -= 1
+#             else:
+#                 # index가 1부터 시작
+#                 return left + 1, right + 1
             
-        
-        
-        
+            
+            
+            
+        # 2. 이진 검색
+        for k, v in enumerate(numbers):
+            left, right = k + 1, len(numbers) - 1
+            expected = target - v
+            # 이진 검색으로 나머지 값 판별
+            while left <= right:
+                mid = left + (right - left) // 2
+                if numbers[mid] < expected:
+                    left = mid + 1
+                elif numbers[mid] > expected:
+                    right = mid - 1
+                else:
+                    return k + 1, mid + 1
+                
+            
