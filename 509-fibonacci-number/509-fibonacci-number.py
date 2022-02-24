4ee97295094750ebcc148efef511b1ad330ed949
Park Cheolsoon <mn5724@naver.com>
2022-02-24 15:42:31 +0900

    Time: 36 ms (72.86%), Space: 13.9 MB (61.22%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -20,11 +20,18 @@ class Solution:
 #         return self.dp[n]
         
         
-        # 3. bottom up
-        self.dp[0] = 0
-        self.dp[1] = 1
+#         # 3. bottom up
+#         self.dp[0] = 0
+#         self.dp[1] = 1
         
-        for i in range(2, n + 1):
-            self.dp[i] = self.dp[i - 1] + self.dp[i - 2]
-        return self.dp[n]
+#         for i in range(2, n + 1):
+#             self.dp[i] = self.dp[i - 1] + self.dp[i - 2]
         
+#         return self.dp[n]
+        
+        
+        # 4. saving memory
+        x, y = 0, 1
+        for i in range(0, n):
+            x, y = y, x+y
+        return x
