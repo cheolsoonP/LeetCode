90d348355ecb1015dab493bf1680ebe28e07f8da
Park Cheolsoon <mn5724@naver.com>
2022-03-07 23:39:39 +0900

    Time: 73 ms (72.42%), Space: 13.9 MB (49.16%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.py
+++ 9-palindrome-number/9-palindrome-number.py
@@ -1,8 +1,6 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        
-        s = list(str(x))
-        
+                
         if list(str(x)) == list(str(x))[::-1]:
             return True
         
